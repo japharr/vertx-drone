d9d0b5482001204481eeb7074e079d348f3d280e
Jelil Adesina <jtutoff@gmail.com>
2022-01-12 17:25:02 +0100

    contain Drone fields and constraints

--- app/src/main/java/com/jeliiadesina/drone/entity/Drone.java
+++ app/src/main/java/com/jeliiadesina/drone/entity/Drone.java
@@ -1,2 +1,18 @@
-package com.jeliiadesina.drone.entity;public class Drone {
+package com.jeliiadesina.drone.entity;
+
+import java.util.List;
+
+public abstract class Drone {
+  // field-name
+  public static final String SERIAL_NUMBER = "serialNumber";
+  public static final String MODEL = "model";
+  public static final String WEIGHT_LIMIT = "weightLimit";
+  public static final String BATTERY_CAPACITY = "batteryCapacity";
+  public static final String STATE = "state";
+
+  // validation
+  public static final int SERIAL_NUMBER_MAX = 100;
+  public static final List<String> ACCEPTABLE_MODELS =
+      List.of("Lightweight", "Middleweight", "Cruiserweight", "Heavyweight");
+  public static final double WEIGHT_LIMIT_MAX = 500.0;
 }
