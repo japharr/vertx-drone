3cc400c100d9809f8d0edeb878c16d861a2c2f5f
Jelil Adesina <jtutoff@gmail.com>
2022-01-12 14:52:29 +0100

    load configure parameters from conf.yaml then  deploy verticles

--- app/src/main/java/com/jeliiadesina/drone/App.java
+++ app/src/main/java/com/jeliiadesina/drone/App.java
@@ -1,7 +1,46 @@
 package com.jeliiadesina.drone;
 
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
-    public static void main(String[] args) {
-        
-    }
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+  private static Vertx vertx;
+
+  public static void main(String[] args) {
+    vertx = Vertx.vertx();
+
+    initConfig()
+        .compose(App::deployVerticle)
+        .onComplete(deployed -> {
+          if (deployed.succeeded()) {
+            logger.info("Verticle(s) successfully deployed");
+          } else {
+            logger.error("Failed to deploy verticle(s)", deployed.cause());
+          }
+        });
+  }
+
+  private static Future<JsonObject> initConfig() {
+    var storeOptions = new ConfigStoreOptions()
+        .setFormat("yaml").setType("file")
+        .setConfig(new JsonObject().put("path", "conf.yaml"));
+
+    var retrievalOptions = new ConfigRetrieverOptions()
+        .addStore(storeOptions);
+
+    var configRetriever = ConfigRetriever.create(vertx, retrievalOptions);
+
+    return configRetriever.getConfig();
+  }
+
+  private static Future<Void> deployVerticle(JsonObject config) {
+    return Future.succeededFuture();
+  }
 }
