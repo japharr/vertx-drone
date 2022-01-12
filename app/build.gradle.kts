19cdeb5eb807e5726d768206d5737b25dbeae8b4
Jelil Adesina <jtutoff@gmail.com>
2022-01-12 13:25:35 +0100

    added the required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,21 @@
 
 dependencies {
+    val vertxVersion = project.extra["vertxVersion"]
+    val flywayVersion = project.extra["flywayVersion"]
+    val jupiterVersion = project.extra["jupiterVersion"]
+    val logbackClassicVersion = project.extra["logbackClassicVersion"]
+    val postgresTestContainerVersion = project.extra["postgresTestContainerVersion"]
 
+    implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-config:$vertxVersion")
+    implementation("io.vertx:vertx-config-yaml:$vertxVersion")
+    implementation("io.vertx:vertx-pg-client:$vertxVersion")
+
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackClassicVersion")
+    implementation("org.testcontainers:postgresql:$postgresTestContainerVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
 }
 
 application {
