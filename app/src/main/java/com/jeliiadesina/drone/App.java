4c4c15d805a0ae6eeb373a70ee63003b3606a6c6
Jelil Adesina <jtutoff@gmail.com>
2022-01-13 05:40:52 +0100

    added DatabaseVerticle to deployed verticles

--- app/src/main/java/com/jeliiadesina/drone/App.java
+++ app/src/main/java/com/jeliiadesina/drone/App.java
@@ -1,9 +1,11 @@
 package com.jeliiadesina.drone;
 
+import com.jeliiadesina.drone.vertcle.DatabaseVerticle;
 import com.jeliiadesina.drone.vertcle.WebVerticle;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
@@ -45,6 +47,9 @@ public class App {
   private static Future<Void> deployVerticle(JsonObject config) {
     var deploymentOpts = new DeploymentOptions().setConfig(config);
 
-    return vertx.deployVerticle(new WebVerticle(), deploymentOpts).mapEmpty();
+    var webVerticle = vertx.deployVerticle(new WebVerticle(), deploymentOpts);
+    var dbVerticle = vertx.deployVerticle(new DatabaseVerticle(), deploymentOpts);
+
+    return CompositeFuture.all(dbVerticle, webVerticle).mapEmpty();
   }
 }
--- app/src/main/java/com/jeliiadesina/drone/entity/Drone.java
+++ app/src/main/java/com/jeliiadesina/drone/entity/Drone.java
@@ -1,8 +1,9 @@
 package com.jeliiadesina.drone.entity;
 
 import java.util.List;
+import java.util.Map;
 
-public abstract class Drone {
+public interface Drone {
   // field-name
   public static final String SERIAL_NUMBER = "serialNumber";
   public static final String MODEL = "model";
@@ -16,9 +17,21 @@ public abstract class Drone {
       List.of("Lightweight", "Middleweight", "Cruiserweight", "Heavyweight");
   public static final double WEIGHT_LIMIT_MAX = 500.0;
 
+  // event-buss addresses
   public static final String REGISTER_ADDRESS = "drone.register";
 
   public enum StateType{
     IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
+  }
+
+  // error codes
+  static Map<Integer, String> ERROR_CODES = Map.of(101, "drone.serialNumber.exist");
+
+  static String insertDrone() {
+    return "INSERT INTO drones VALUES($1, $2, $3, $4, $5, $6, current_timestamp, current_timestamp)";
+  }
+
+  static String countBySerialNumber() {
+    return "SELECT count(*) FROM drones WHERE serial_number = $1";
   }
 }
