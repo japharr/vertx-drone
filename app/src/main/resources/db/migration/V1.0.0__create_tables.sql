f98d2553ab62be98a703cccede1fb1fef1d5948c
Jelil Adesina <jtutoff@gmail.com>
2022-01-13 05:39:45 +0100

    create tables migration sqls

---
+++ app/src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE drones (
+uuid VARCHAR(500) NOT NULL,
+serial_number VARCHAR(100) NOT NULL,
+model VARCHAR(100) NOT NULL,
+weight_limit DECIMAL NOT NULL,
+battery_capacity DECIMAL NOT NULL,
+state VARCHAR(100) NOT NULL,
+created_date TIMESTAMP,
+last_modified_date TIMESTAMP
+);
+
+ALTER TABLE drones ADD CONSTRAINT pk_drone_id PRIMARY KEY (uuid);
+
+CREATE TABLE medications (
+uuid VARCHAR(500) NOT NULL,
+name VARCHAR(100) NOT NULL,
+code VARCHAR(100) NOT NULL,
+weight DECIMAL NOT NULL,
+image VARCHAR(100),
+created_date TIMESTAMP,
+last_modified_date TIMESTAMP
+);
+
+ALTER TABLE medications ADD CONSTRAINT pk_medication_id PRIMARY KEY (uuid);
